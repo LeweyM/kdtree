7e159c7d8053bb9532ab44bb279a69e143e0afd9
Lewis Bonathan Metcalf <lewis.metcalf@thoughtworks.com>
2020-06-03 12:27:25 +0200

    implement brute force version of pointset

--- src/PointSET.java
+++ src/PointSET.java
@@ -1,38 +1,63 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.util.ArrayList;
 
 public class PointSET {
-    public PointSET() {
 
+    private final SET<Point2D> points;
+
+    public PointSET() {
+        points = new SET<>();
     }
 
     public boolean isEmpty() {
-        return false;
+        return points.isEmpty();
     }
 
     public int size() {
-        return 0;
+        return points.size();
     }
 
     public void insert(Point2D p) {
-
+        points.add(p);
     }
 
     public boolean contains(Point2D p) {
-        return false;
+        return points.contains(p);
     }
 
     public void draw() {
-
+        for (Point2D p : points) {
+            StdDraw.point(p.x(), p.y());
+        }
     }
 
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        ArrayList<Point2D> pointsInRect = new ArrayList<>();
+        for (Point2D p : points) {
+            if (   p.x() >= rect.xmin()
+                && p.x() <= rect.xmax()
+                && p.y() >= rect.ymin()
+                && p.y() <= rect.ymax()) {
+                pointsInRect.add(p);
+            }
+        }
+        return pointsInRect;
     }
 
     public Point2D nearest(Point2D p) {
-        return null;
+        Point2D closestPoint = null;
+        for (Point2D point : points) {
+            if (p != point && (closestPoint == null || point.distanceTo(p) < closestPoint.distanceTo(p))) {
+                closestPoint = point;
+            }
+        }
+        return closestPoint;
     }
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
 }
--- test/PointSETTest.java
+++ test/PointSETTest.java
@@ -1,11 +1,56 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointSETTest {
 
+    private PointSET pSet;
+
+    @BeforeEach
+    void setUp() {
+        pSet = new PointSET();
+    }
+
     @Test
-    void sanity_test() {
-        assertEquals(1, 1);
+    void is_empty_and_size() {
+        assertTrue(pSet.isEmpty());
+        insertPoint(0, 0);
+        assertFalse(pSet.isEmpty());
+        assertEquals(1, pSet.size());
+    }
+
+    @Test
+    void contains() {
+        Point2D p = new Point2D(0, 0);
+        assertFalse(pSet.contains(p));
+        pSet.insert(p);
+        assertTrue(pSet.contains(p));
+    }
+
+    @Test
+    void nearest_point() {
+        insertPoint(0, 0);
+        insertPoint(0, 1);
+        insertPoint(1, 1);
+        assertEquals(pSet.nearest(new Point2D(0.1, 0.1)), new Point2D(0, 0));
+    }
+
+    @Test
+    void range() {
+        insertPoint(0, 0);
+        insertPoint(0.5, 0.5);
+        insertPoint(0, 1);
+        Iterable<Point2D> range = pSet.range(new RectHV(0.2, 0.2, 0.8, 0.8));
+        assertThat(range, Matchers.contains(new Point2D(0.5, 0.5)));
+        assertEquals(range.spliterator().getExactSizeIfKnown(), 1);
+    }
+
+    private void insertPoint(double x, double y) {
+        pSet.insert(new Point2D(x, y));
     }
 }
